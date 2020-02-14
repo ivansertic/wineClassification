9ae4735d696c6755c84ea9e3638b5f18d68f7f4e
ivansertic <serticivan5@gmail.com>
2020-02-14 18:17:10 +0100

    added api functionality

---
+++ ruapProjectFormApp/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ruapProjectFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblResidualShugar = new System.Windows.Forms.Label();
+            this.lblAlcohol = new System.Windows.Forms.Label();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.tbResidualShugar = new System.Windows.Forms.TextBox();
+            this.tbAlcohol = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(66, 87);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(31, 13);
+            this.lblType.TabIndex = 0;
+            this.lblType.Text = "Type";
+            // 
+            // lblResidualShugar
+            // 
+            this.lblResidualShugar.AutoSize = true;
+            this.lblResidualShugar.Location = new System.Drawing.Point(66, 127);
+            this.lblResidualShugar.Name = "lblResidualShugar";
+            this.lblResidualShugar.Size = new System.Drawing.Size(105, 13);
+            this.lblResidualShugar.TabIndex = 1;
+            this.lblResidualShugar.Text = "Residual shugar [g/l]";
+            // 
+            // lblAlcohol
+            // 
+            this.lblAlcohol.AutoSize = true;
+            this.lblAlcohol.Location = new System.Drawing.Point(66, 168);
+            this.lblAlcohol.Name = "lblAlcohol";
+            this.lblAlcohol.Size = new System.Drawing.Size(53, 13);
+            this.lblAlcohol.TabIndex = 2;
+            this.lblAlcohol.Text = "Alcohol %";
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Location = new System.Drawing.Point(299, 311);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(75, 23);
+            this.btnSendData.TabIndex = 3;
+            this.btnSendData.Text = "Send Data";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(218, 311);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 4;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // cbType
+            // 
+            this.cbType.AllowDrop = true;
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "red",
+            "white"});
+            this.cbType.Location = new System.Drawing.Point(218, 79);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 21);
+            this.cbType.TabIndex = 5;
+            this.cbType.Text = "red";
+            // 
+            // tbResidualShugar
+            // 
+            this.tbResidualShugar.Location = new System.Drawing.Point(218, 120);
+            this.tbResidualShugar.Name = "tbResidualShugar";
+            this.tbResidualShugar.Size = new System.Drawing.Size(121, 20);
+            this.tbResidualShugar.TabIndex = 6;
+            // 
+            // tbAlcohol
+            // 
+            this.tbAlcohol.Location = new System.Drawing.Point(218, 160);
+            this.tbAlcohol.Name = "tbAlcohol";
+            this.tbAlcohol.Size = new System.Drawing.Size(121, 20);
+            this.tbAlcohol.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 346);
+            this.Controls.Add(this.tbAlcohol);
+            this.Controls.Add(this.tbResidualShugar);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSendData);
+            this.Controls.Add(this.lblAlcohol);
+            this.Controls.Add(this.lblResidualShugar);
+            this.Controls.Add(this.lblType);
+            this.Name = "Form1";
+            this.Text = "Wine Classification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblResidualShugar;
+        private System.Windows.Forms.Label lblAlcohol;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.TextBox tbResidualShugar;
+        private System.Windows.Forms.TextBox tbAlcohol;
+    }
+}
+
---
+++ ruapProjectFormApp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ruapProjectFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String wineType;
+        private float residualShugars;
+        private float alcohol;
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void btnSendData_Click(object sender, EventArgs e)
+        {
+            this.wineType = cbType.Text.ToString();
+
+            String residualShugar = tbResidualShugar.Text.ToString();
+            residualShugar = residualShugar.Replace(',', '.');
+            bool valid = float.TryParse(residualShugar, out this.residualShugars);
+
+            if(!valid)
+            {
+                MessageBox.Show("Input residual shugar is not a number!");
+                Application.Exit();
+            }
+
+            String alcoholPercent = tbAlcohol.Text.ToString();
+            alcoholPercent = alcoholPercent.Replace(',', '.');
+            bool alcoholValid = float.TryParse(alcoholPercent, out this.alcohol);
+
+            if(!alcoholValid)
+            {
+                MessageBox.Show("Input alcohol is not a number!");
+                Application.Exit();
+            }
+
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>() {
+                        {
+                            "input1",
+                            new List<Dictionary<string, string>>(){new Dictionary<string, string>(){
+                                            {
+                                                "type", this.wineType.ToString()
+                                            },
+                                            {
+                                                "residual sugar", this.residualShugars.ToString()
+                                            },
+                                            {
+                                                "alcohol", this.alcohol.ToString()
+                                            },
+                                }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                const string apiKey = "KdmwGM+K5JSSFK7CBiy6gPlmGM5ChZMnjxNyd3v74h+hnqf5G9Tuot0kvh6FtgylElzkVAMEwgHs0rzzmKuUIg==";
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/7fe579a4046e4762a82c2e35ed3b4fd0/services/0681a2249a5849e0828dc7dfd5b79609/execute?api-version=2.0&format=swagger");
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    dynamic jsonResult = JValue.Parse(result);
+
+                    string wineTypeOutput = jsonResult.Results.output1[0].type.ToString();
+
+                    if (jsonResult.Results.output1[0]["Scored Labels"] == 1)
+                    {
+                        float badWine = jsonResult.Results.output1[0]["Scored Probabilities for Class \"1\""];
+                        badWine *= 100;
+                        MessageBox.Show("It's a bad "+ wineTypeOutput+ " wine and I'm " + badWine + " % sure of it", "Results");
+                    }
+
+                    if (jsonResult.Results.output1[0]["Scored Labels"] == 2)
+                    {
+                        float goodWine = jsonResult.Results.output1[0]["Scored Probabilities for Class \"2\""];
+                        goodWine *= 100;
+                        MessageBox.Show("It's a good " + wineTypeOutput + " wine and I'm " + goodWine + " % sure of it" , "Results");
+                    }
+
+                    if (jsonResult.Results.output1[0]["Scored Labels"] == 3)
+                    {
+                        float veryGoodWine = jsonResult.Results.output1[0]["Scored Probabilities for Class \"3\""];
+                        veryGoodWine *= 100;
+                        MessageBox.Show("It's a very good "+ wineTypeOutput + " wine and I'm "+ veryGoodWine + " sure of it", "Results");
+                    }
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show("The request failed with status code: {0}" + response.StatusCode, "Error", MessageBoxButtons.OK);
+                }
+            }
+
+            
+
+        }
+    }
+}
